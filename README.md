e5b77278226d469875dd6f34ada558f55b032a1d
Artsiom Zhurbila <artemzhurbilo@gmail.com>
2014-10-31 17:13:12 +0300

    -change build.sh for travis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/azhurbilo/base-ubuntu.svg?branch=master)](https://travis-ci.org/azhurbilo/base-ubuntu)
+
 [![Galaxy](http://img.shields.io/badge/galaxy-zhurbilo.base--ubuntu-blue.svg?style=flat-square)](https://galaxy.ansible.com/list#/roles/2046)
 
 base-ubuntu (Ansible role)
--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ echo ">>> Step 2: run the role/playbook with ansible-playbook"
 ansible-playbook -i ${INVENTORY_FILE} playbook.yml
 
 # Step 3:
-echo ">>> Step 4: run the role/playbook again, checking to make sure it's idempotent."
+echo ">>> Step 3: run the role/playbook again, checking to make sure it's idempotent."
 echo ">>> Ansible..."
 ansible-playbook -i ${INVENTORY_FILE} playbook.yml > out.txt 2>&1
 
@@ -39,8 +39,12 @@ cat out.txt
 cat out.txt | grep -q 'changed=0.*failed=0'\
 && (echo 'Idempotence test: Success' && rm out.txt && exit 0) || (echo 'Idempotence test: Fail' && rm out.txt && exit 1)
 
+
+if [ "$1" = "vagrant" ]
+then
+
 # Step 4:
-echo ">>> Step 5: run the role/playbook again, after machine reboot to make sure it's idempotent as well =)"
+echo ">>> Step 4: run the role/playbook again, after machine reboot to make sure it's idempotent as well =)"
 vagrant reload
 echo ">>> Ansible..."
 ansible-playbook -i ${INVENTORY_FILE} playbook.yml > out.txt 2>&1
@@ -49,6 +53,10 @@ cat out.txt
 
 cat out.txt | grep -q 'changed=0.*failed=0'\
 && (echo 'Idempotence test after reboot: Success' && rm out.txt && exit 0) || (echo 'Idempotence test after reboot: Fail' && rm out.txt && exit 1)
+
+fi
+
+
 
 echo "=================="
 echo "SUCCESS"
